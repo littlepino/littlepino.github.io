94d72adbc5873b04610b215f447d981dc87c6987
xfy <793979816@qq.com>
2019-08-07 15:41:18 +0800

    一年后的我删除了一些代码

--- text-site/scripts/main.js
+++ text-site/scripts/main.js
@@ -10,20 +10,8 @@ myImage.onclick = function () {
     }
     
 }
-var iceCream='chocolate';
-if (iceCream==='chocolate'){
-    alert('Yay,I love chocolate ice cream');
-} else {
-    alert("emmmm,but chocolate is my favorite...")
-}
-function mutiply(num1,num2) {
-    var reslut=num1*num2;
-    return reslut;
-    
-}
-document.querySelector('html').onclick=function () {
-    alert('oh,stop poking me ');
-}
+
+
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
@@ -31,6 +19,9 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
+    if(!myNmae){
+        return myHeading.textContent = '帅气的星爷'
+    }
     myHeading.textContent = '帅气的星爷, ' + myName;
 }
 if(!localStorage.getItem('name')) {
